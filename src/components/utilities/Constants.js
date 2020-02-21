1811a9cde5d2f09cf0e4ac5716e79800d1cad686
Andrew Joy <56839275+AndrewJoyT@users.noreply.github.com>
2020-02-21 15:43:15 -0500

    oke

---
+++ src/components/utilities/Constants.js
@@ -0,0 +1,33 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+module.exports = {
+  URL: {
+    BASE_URL: "https://api.themoviedb.org/3/",
+    IMAGE_URL: "http://image.tmdb.org/t/p/w185",
+    API_KEY: "api_key=151dfa1b4c6a83a02970c0c6612615b3",
+    SEARCH_QUERY: "search/movie?query=",
+    PLACEHOLDER_IMAGE: "https://s3-ap-southeast-1.amazonaws.com/popcornsg/placeholder-movieimage.png"
+  },
+  
+  Strings: {
+    MAIN_TITLE: "TMDb",
+    SECONDARY_TITLE: "Detail Movie",
+    PLACEHOLDER: "Cari Film",
+    SEARCH_BUTTON: "Search",
+    RELEASE_DATE: "Tanggal Rilis: ",
+    LANGUAGE: "Bahasa: ",
+    POPULARITY: "Popularitas: ",
+    STATUS: "Status: ",
+    RATINGS: "Peringkat: ",
+    POPULARITY : "Popularitas: ",
+    BUDGET: "Anggaran: ",
+    REVENUE: "Pendapatan: ",
+    RUNTIME: "Durasi: ",
+    OVERVIEW: "Gambaran: ",
+    MSG: "Silahkan Isi Terlebih Dahulu."
+  },
+  Colors: {
+    Cyan: "#02ADAD",
+    Grey: "#EDEDED",
+    Transparent: "transparent"
+  }
+};
